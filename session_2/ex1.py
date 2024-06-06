a2f10c5f1f3ed7cad56ad92f03f9ce910e88250f
thoattuc <thoattuc.hd@gmail.com>
2024-06-06 22:48:34 +0700

    Commit

---
+++ session_2/ex1.py
@@ -0,0 +1,5 @@
+"""
+    Yêu cầu người dùng nhập vào 1 số tự nhiên lớn hơn 1
+    1. Kiểm tra xem đó có phải là số nguyên tố hay không
+    2. In ra màn hình tất cả các số nguyên tố nhỏ hơn hoặc bằng n
+"""
