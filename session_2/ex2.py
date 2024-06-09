c2ac02529e3bda030307babe6869bd013fe6b9f7
thoattuc <thoattuc.hd@gmail.com>
2024-06-09 15:53:39 +0700

    Commit

---
+++ session_2/ex2.py
@@ -0,0 +1,24 @@
+"""
+    Yêu cầu người dùng nhập vào 1 số tự nhiên n khác 0
+    Tính n! theo 2 cách:
+    - Sử dụng vòng lặp for
+    - Sử dụng vòng lặp while
+"""
+
+# Use for-loop
+n = int(input('[for-loop] Enter the number: '))
+result = 1
+for i in range(1, n+1):
+    result = result * i
+
+print('The factorial of ', n, 'is', result)
+
+# Use while-loop
+a = int(input('[while-loop] Enter the number: '))
+factorial = 1
+count = 1
+while count <= a:
+    factorial *= count
+    count += 1
+
+print('The factorial of ', a, 'is', factorial)
