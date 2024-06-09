5805c55a0b0190c668737a4bf9a9c20812b0e90c
thoattuc <thoattuc.hd@gmail.com>
2024-06-10 06:02:24 +0700

    Commit

---
+++ session_5/dictionary.py
@@ -0,0 +1,18 @@
+# Dictionary empty:
+data_1 = {}
+# Dictionary with key/value:
+data_2 = {"name": "Niam", "age": "29", "job": "dev"}
+# Dictionary with key is argument:
+data_3 = dict(name="Niam", age="29", job="dev")
+# Dictionary with a list of tuples:
+data_4 = dict([("name", "Niam"), ("age", "29"), ("job", "dev")])
+# Dictionary with a tuple of lists
+data_5 = dict((["name", "Niam"], ["age", "29"], ["job", "dev"]))
+
+A = {"day": "Sunday", "date": "080595", "Day": "Monday"}
+print(A)
+
+# Element access:
+
+
+
