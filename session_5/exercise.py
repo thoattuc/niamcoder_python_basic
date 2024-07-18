f778aafb3236025932f3b8e51263de8471b9030b
thoattuc <thoattuc.hd@gmail.com>
2024-07-18 07:23:13 +0700

    Commit

--- session_5/exercise.py
+++ session_5/exercise.py
@@ -46,3 +46,43 @@ if __name__ == "__main__":
     ]
 
     print(add_matrices(mat1, mat2))
+
+"""
+    Yêu cầu người dùng nhập vào 1 số nguyên dương
+    Kiểm tra xem số đó có phải là 1 số may mắn hay không
+
+    Số may mắn là số được định nghĩa theo quá trình sau: bắt đầu với số nguyên dương x
+    và tính tổng bình phương y các chữ số của x, sau đó tiếp tục tính tổng bình phương
+    các chữ số của y. Quá trình này lặp đi lặp lại cho đến khi thu được kết quả là 1
+    thì dừng (tổng bình phương các chữ số của số 1 chính là 1) hoặc quá trình sẽ kéo dài vô tận.
+    Số mà quá trình tính này kết thúc bằng 1 gọi là số may mắn.
+    Số có quá trình tính kéo dài vô tận là số không may mắn hay còn gọi là số đen đủi
+    Ví dụ: 19 là số may mắn vì
+    1^2 + 9^2 = 82
+    8^2 + 2^2 = 68
+    6^2 + 8^2 = 100
+    1^2 + 0^2 + 0^2 = 1 (End)
+
+    Some happy numbers are: 1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63, 67, 69, 73, 75, 79, 87, 93, 99, 105,
+    111, 115, 127, 129, 133, 135, 141, 151, 159, 163, 169, 171, 189, 193, 195, 201, 205, 211, 219, 223, 231, 235, 237,
+    241, 259, 261, 267, 273, 283, 285, 289, 297, 303, 307, 319, 321, 327, 331
+"""
+
+
+def check_happy_number(number):
+    sum_history = []
+    while True:
+        if number in sum_history:
+            return False
+        sum_history.append(number)
+        sum_of_digits = 0
+        for item in number:
+            sum_of_digits += int(item) ** 2
+        if sum_of_digits == 1:
+            return True
+        number = str(sum_of_digits)
+
+
+n = input("Enter a number: ")
+
+print(check_happy_number(n))
