599825fe8fcce8a5676b05df99572335ef94d40a
thoattuc <thoattuc.hd@gmail.com>
2024-07-22 07:33:26 +0700

    Commit

--- session_6/session_6.py
+++ session_6/session_6.py
@@ -59,3 +59,60 @@ int_value = 1995
 str_value = str(int_value)
 print(str_value)
 print(type(int_value))
+
+print(data_str_1[1])
+print(data_str_2[2:5])
+
+print(data_str_3[::-1])
+
+new_str = "P" + data_str_3[1:]
+print(new_str)
+
+string_1 = "hello" + "world"
+print(string_1)
+
+string_1 += "I start learning Python today"
+print(string_1)
+
+string_2 = "hello" "world" "!"
+print(string_2)
+
+string_2 = string_2 * 5
+print(string_2)
+
+print(len(string_2))
+
+string_1 = string_1.replace("today", "every day!")
+print(string_1)
+
+print(string_1.lower())
+
+print(string_2.upper())
+
+print(string_1.capitalize())
+
+print(string_2.title())
+
+# escape characters: [\+character]
+print("\'")
+print("\\")
+print("\"")
+print("\n")
+print("\t tab")
+
+string_3 = "   Today is Sunday "
+string_3 = string_3.strip()
+print(string_3)
+
+# Format String:
+string_4 = input("Please, input info: ")
+new_string = "....{}....".format(string_4)
+print(new_string)
+
+new_string = "....{1}....{0}....{2}....".format("hello", "world", "!")
+print(new_string)
+
+string_5 = "hello world !"
+print(string_5.split())
+
+
