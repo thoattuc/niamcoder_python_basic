97e0ca644ebe47b49fbd2a16dab842d31b0015c7
thoattuc <thoattuc.hd@gmail.com>
2024-08-08 02:44:25 +0700

    Commit

--- session_5/session_5.py
+++ session_5/session_5.py
@@ -13,6 +13,50 @@ A = {"day": "Sunday", "date": "080595", "Day": "Monday"}
 print(A)
 
 # Element access:
+data = {"name": "Niam", "age": "29", "job": "dev"}
+print(data["name"])
+print(data.get("age"))  # tranh' bao' loi
+
+# Update
+data["gender"] = "Male"
+data["age"] = 30
+print(data)
+
+# Delete:
+removed_item = data.pop("gender")
+print(data)
+
+del data["age"]
+print(data)
+
+data.clear()
+print(data)
+
+# value, item,  key access:
+data = {"name": "Niam", "age": "29", "job": "dev"}
+print(data.values())  # dict:
+print(data.keys())  # key
+print(data.items())  # item
+
+print(len(data))
+
+# immutable:
+a = 50
+print(id(a))    # ...x
+
+a = 60
+print(id(a))    # ...y
+
+# mutable:
+list_number = [1, 2, 3, 5, 8]
+print(id(list_number))  # ...z
+
+list_number.append(13)
+print(id(list_number))  # ...z
+
+
+
+
 
 # Tuple packing:
 data_tuples = ("red", "green", "yellow", "blue", "black")   # 5 values
